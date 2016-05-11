45a9d32a112acdae0e690e5688da7150f102c204
petrkubat <nando128@gmail.com>
2016-05-11 17:15:44 +0200

    Added basic interface files.

--- src/control/EncryptionServiceInterface.java
+++ src/control/EncryptionServiceInterface.java
@@ -6,9 +6,9 @@ import java.util.List;
  * Created by petrkubat on 05/05/16.
  */
 public interface EncryptionServiceInterface {
-    public EncryptedFileStatus addFile(RawFile file, String path, String password);
-    public EncryptedFileStatus moveFile(String oldPath, String newPath, String password);
-    public List<EncryptedFileStatus> listFiles(String path, String password);
-    public boolean deleteFile(String path, String password);
-    public RawFile getFile(String path, String password);
+    EncryptedFileStatus addFile(RawFile file, String path, String password);
+    EncryptedFileStatus moveFile(String oldPath, String newPath, String password);
+    List<EncryptedFileStatus> listFiles(String path, String password);
+    boolean deleteFile(String path, String password);
+    RawFile getFile(String path, String password);
 }
