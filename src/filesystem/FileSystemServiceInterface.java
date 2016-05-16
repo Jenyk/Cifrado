d4f0fc3ca82e8ad3c36309651f9eacae3fad9160
petrkubat <nando128@gmail.com>
2016-05-16 16:41:39 +0200

    Implemented LocalFileStorage

--- src/filesystem/FileSystemServiceInterface.java
+++ src/filesystem/FileSystemServiceInterface.java
@@ -1,6 +1,10 @@
 package filesystem;
 
+import filesystem.exceptions.InvalidPathException;
+import filesystem.exceptions.PathCollisionException;
+
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
@@ -8,9 +12,10 @@ import java.util.List;
  * Created by petrkubat on 05/05/16.
  */
 public interface FileSystemServiceInterface {
-    File createFile(InputStream content, String path);
-    List<File> listFiles(String path);
-    File getFile(String path);
+    void initialize() throws IOException;
+    File createFile(InputStream content, String path) throws InvalidPathException, PathCollisionException, IOException;
+    List<File> listFiles(String path) throws InvalidPathException, IOException;
+    File getFile(String path) throws InvalidPathException;
     void moveFile(String oldPath, String newPath);
-    void deleteFile(String path);
+    void deleteFile(String path) throws InvalidPathException, IOException;
 }
