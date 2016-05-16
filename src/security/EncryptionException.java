8f0a5b74f05187ca1c3f07035fd97e4570090ec2
Jan Holman <jenyk.holman@gmail.com>
2016-05-16 15:57:45 +0200

    EAS encryption

---
+++ src/security/EncryptionException.java
@@ -0,0 +1,12 @@
+package security;
+
+/**
+ * Created by jan on 16.5.16.
+ */
+public class EncryptionException extends Exception {
+
+    public EncryptionException(String message) {
+        super(message);
+
+    }
+}
